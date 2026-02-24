cb53e55554437ccdd1b02d7af89fe2b3bcdd5f13
K AKHIL KUMAR <140427573+Akhilkumar24@users.noreply.github.com>
2026-02-24 14:32:59 +0530

    Update reflections.routes.ts

--- backend/src/routes/v1/reflections.routes.ts
+++ backend/src/routes/v1/reflections.routes.ts
@@ -1,1 +1,17 @@
 
+import { Router } from "express";
+import {
+  getReflections,
+  getReflectionsByOutcome,
+  postReflection,
+  getReflectionByIdController,
+} from "../controllers/reflections.controller";
+
+const router = Router();
+
+router.post("/", postReflection);
+router.get("/", getReflections);
+router.get("/id/:id", getReflectionByIdController);
+router.get("/:outcomeId", getReflectionsByOutcome);
+
+export default router;
